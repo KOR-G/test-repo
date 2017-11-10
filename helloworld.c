9257515b6aaa6beb727d99e2022920885213efcc
KOR-G <33534941+KOR-G@users.noreply.github.com>
2017-11-10 12:04:20 +0900

    GitHub for git pull (2017.11.10)

--- helloworld.c
+++ helloworld.c
@@ -1,6 +1,6 @@
 // For command line git tutorial
 // First: GitHub modification
-// Secon: Local repository modification
+// Second: Local repository modification
 #include <stdio.h>
 
 int main(void) {
@@ -8,7 +8,8 @@ int main(void) {
 	printf("Hello World\n");
 	printf("Hello Your World\n");
 	printf("Hellow his World\n");
-	printf("Hellow her world\n");
+	printf("Hellow her World\n");
+	printf("Hellow my World\n");
 
 	return 0;
 }
